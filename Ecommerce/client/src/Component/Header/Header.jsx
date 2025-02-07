e07c8f9d5850c79b2dcbf3fbd2b9bb669fad29cc
gawali9405 <gawali9405@gmail.com>
2025-02-07 20:34:24 +0530

    start

---
+++ Ecommerce/client/src/Component/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+   <>
+  <header>
+  <div className="logo">
+    <img src="#" alt="" />
+    </div>  
+
+    <nav className='text-green-500 bg-amber-300'>
+     eaidsfm,s,k
+    </nav>
+  </header>   
+   </>
+  )
+}
+
+export default Header
